b6a5b86be4f22bdfc1c0e7ed04bec7ae44b8c863
tiany <908673267@qq.com>
2023-04-04 22:28:16 +0800

    创建工程

---
+++ src/main/java/org/ty/herostory/ServerMain.java
@@ -0,0 +1,50 @@
+package org.ty.herostory;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
+
+/**
+ * 工程主函数
+ */
+public class ServerMain {
+
+    public static void main(String[] args) {
+        EventLoopGroup boosGroup = new NioEventLoopGroup(); // 处理客服端的连接
+        EventLoopGroup workerGroup = new NioEventLoopGroup();// 工作线程池
+
+        ServerBootstrap b = new ServerBootstrap();
+        b.group(boosGroup, workerGroup);
+        b.channel(NioServerSocketChannel.class);
+        b.childHandler(new ChannelInitializer<SocketChannel>() {
+            @Override
+            protected void initChannel(SocketChannel ch) throws Exception {
+                ch.pipeline().addLast(
+                        new HttpServerCodec(),
+                        new HttpObjectAggregator(65535),
+                        new WebSocketServerProtocolHandler("/websocket")
+                );
+            }
+        });
+
+        try {
+            ChannelFuture f = b.bind(12345).sync();
+
+            if(f.isSuccess()){
+                System.out.println("服务器启动成功");
+            }
+
+            f.channel().closeFuture().sync();
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+}
